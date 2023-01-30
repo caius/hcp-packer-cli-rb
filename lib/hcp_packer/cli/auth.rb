7cbfd10ec03b88bbdb5c8d87adfe4d8f0dea209c
Caius Durling <dev@caius.name>
2023-01-30 19:43:23 +0000

    Dump out auth for now

--- lib/hcp_packer/cli/auth.rb
+++ lib/hcp_packer/cli/auth.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
+require "net/http"
+require "json"
+
 module HCPPacker
   module CLI
     class Auth < Dry::CLI::Command
@@ -9,7 +12,22 @@ module HCPPacker
       end
 
       def call
-        puts "TODO: implement authentication"
+
+        url = URI("https://auth.hashicorp.com/oauth/token")
+        Net::HTTP.start(url.host, url.port, use_ssl: true) do |http|
+          req = Net::HTTP::Post.new(url)
+          req["Content-Type"] = "application/json"
+          req.body = JSON.dump({
+            audience: "https://api.hashicorp.cloud",
+            grant_type: "client_credentials",
+            client_id: ENV.fetch("HCP_CLIENT_ID"),
+            client_secret: ENV.fetch("HCP_CLIENT_SECRET"),
+          })
+
+          res = http.request(req)
+          p res, res.body
+        end
+
       end
     end
   end
